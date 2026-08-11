bfaef65d3fe1a4b318b2f1ce2569bf23413eae61
kislit <132556711+Vamp1reAchao@users.noreply.github.com>
2026-08-11 09:54:10 +0800

    test: cover all unsupported payment methods

--- tests/001_test_payment_method.py
+++ tests/001_test_payment_method.py
@@ -8,16 +8,21 @@ from pyfragment.exceptions import ConfigurationError
 
 
 @pytest.mark.asyncio
-async def test_purchase_stars_rejects_unsupported_payment_method() -> None:
+@pytest.mark.parametrize(
+    "payment_method",
+    [
+        PaymentMethod.USDT_ETH,
+        PaymentMethod.USDT_POL,
+        PaymentMethod.USDC_ETH,
+        PaymentMethod.USDC_BASE,
+        PaymentMethod.USDC_POL,
+    ],
+)
+async def test_purchase_stars_rejects_unsupported_payment_method(payment_method: PaymentMethod) -> None:
     client = MagicMock()
     client.call = AsyncMock()
 
     with pytest.raises(ConfigurationError):
-        await purchase_stars(
-            client,
-            "@user",
-            amount=500,
-            payment_method=PaymentMethod.USDT_ETH,
-        )
+        await purchase_stars(client, "@user", amount=500, payment_method=payment_method)
 
     client.call.assert_not_awaited()
